61a2b56e39ad86d9432b3d9e5bae94ce76fbbf96
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-16 15:52:44 +0530

    [ShubhamPV] Compare2Lines

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -10,18 +10,41 @@ namespace Line_Comparision
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter first point :");
-            float x1 = float.Parse(Console.ReadLine()); //converting string value to float value
-            Console.WriteLine("Enter Second point :");
+            Console.WriteLine("Enter first point of first line :");
+            float x1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second point  of first line :");
             float y1 = float.Parse(Console.ReadLine()); ;
-            Console.WriteLine("Enter Third point :");
+            Console.WriteLine("Enter Third point  of first line :");
             float x2 = float.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Fourth point :");
+            Console.WriteLine("Enter Fourth point  of first line :");
             float y2 = float.Parse(Console.ReadLine());
             float v = x2 - x1;
             float w = y2 - y1;
-            Console.WriteLine("The length of a line is :");
-            Console.WriteLine(System.Math.Sqrt(v * v + w * w));
+            float x = v * v + w * w;
+            Console.WriteLine("The length of a first line is :");
+            Console.WriteLine(System.Math.Sqrt(x));
+
+            Console.WriteLine("Enter first point of second line :");
+            float a1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Second point of second line :");
+            float b1 = float.Parse(Console.ReadLine()); ;
+            Console.WriteLine("Enter Third point of second line :");
+            float a2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Fourth pointof second line :");
+            float b2 = float.Parse(Console.ReadLine());
+            float p = a2 - a1;
+            float q = b2 - b1;
+            float r = p * p + q * q;
+            Console.WriteLine("The length of a second line is :");
+            Console.WriteLine(System.Math.Sqrt(r));
+            if (x == r)
+            {
+                Console.WriteLine("Both the lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("Both the lines are not equal");
+            }
             Console.ReadLine();
         }
     }
